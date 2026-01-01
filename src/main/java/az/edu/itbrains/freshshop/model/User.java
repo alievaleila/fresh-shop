ce99fcf62bae12e78540befdcc1b0e90b9e4c57a
alievaleila <meliyeva452@gmail.com>
2026-01-01 23:14:14 +0400

    feat(security): implement Custom UserDetailsService and Security Configuration

--- src/main/java/az/edu/itbrains/freshshop/model/User.java
+++ src/main/java/az/edu/itbrains/freshshop/model/User.java
@@ -1,13 +1,21 @@
 package az.edu.itbrains.freshshop.model;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Data
 @NoArgsConstructor
@@ -15,12 +23,19 @@ import lombok.NoArgsConstructor;
 @Entity
 @Table(name = "users")
 public class User {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String username;
     private String email;
     private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(
+            name = "user_role",
+            joinColumns = @JoinColumn(name = "users", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "roles", referencedColumnName = "id")
+    )
+    private Set<Role> roles = new HashSet<>();
 
 }
